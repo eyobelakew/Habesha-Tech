944369ac29794897bc4e2f4dbb867cf08b8140f7
Daniel <danielyetwale02@gmail.com>
2024-02-04 17:36:25 +0300

    edited some infos on contact us

--- contact-us.php
+++ contact-us.php
@@ -91,7 +91,7 @@
               <i class="bi bi-envelope flex-shrink-0"></i>
               <div>
                 <h4>Email:</h4>
-                <p>info@habeshatech.org</p>
+                <p>habesha@tech.com</p>
               </div>
             </div><!-- End Info Item -->
 
@@ -99,7 +99,7 @@
               <i class="bi bi-phone flex-shrink-0"></i>
               <div>
                 <h4>Call:</h4>
-                <p>+1 5589 55488 55</p>
+                <p>+251923467881</p>
               </div>
             </div><!-- End Info Item -->
 
