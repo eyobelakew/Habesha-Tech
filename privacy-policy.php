ad5a5d00c4a80307cc2a4137a2f1a37a81c2b2f4
Daniel <danielyetwale02@gmail.com>
2024-02-08 09:56:12 +0300

    major change on privacy and policy

--- privacy-policy.php
+++ privacy-policy.php
@@ -82,17 +82,17 @@
     
     <p>This Privacy Policy applies only to our online activities and is valid for visitors to our website and mobile app users with regards to the information that they shared and/or collect in Habesha Tech Developers. This policy is not applicable to any information collected offline or via channels other than this website.</p>
     
-    <h2>Consent</h2>
+    <h5>Consent</h5>
     <p>By using our website or mobile apps, you hereby consent to our Privacy Policy and agree to its terms.</p>
     
-    <h2>Information We Collect</h2>
+    <h5>Information We Collect</h5>
     <p>The personal information that you are asked to provide, and the reasons why you are asked to provide it, will be made clear to you at the point we ask you to provide your personal information.</p>
     
     <p>If you contact us directly, we may receive additional information about you such as your name, email address, phone number, the contents of the message and/or attachments you may send us, and any other information you may choose to provide.</p>
     
     <p>When you register for an Account, we may ask for your contact information, including items such as name, company name, address, email address, and telephone number.</p>
     
-    <h2>How We Use Your Information</h2>
+    <h5>How We Use Your Information</h5>
     <p>We use the information we collect in various ways, including to:</p>
     <ul>
         <li>Provide, operate, and maintain our website</li>
@@ -103,15 +103,15 @@
         <li>Send you emails</li>
         <li>Find and prevent fraud</li>
     </ul>
-    <h2>LOG FILES</h2>
+    <h5>LOG FILES</h5>
     <p>
 Habesha Tech Developers follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting services’ analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users’ movement on the website, and gathering demographic information.</p>
 
-<h2>COOKIES AND WEB BEACONS</h2>
+<h5>COOKIES AND WEB BEACONS</h5>
 <p>
 Like any other website, Habesha Tech Developers uses ‘cookies’. These cookies are used to store information including visitors’ preferences, and the pages on the website that the visitor accessed or visited. The information is used to optimize the users’ experience by customizing our web page content based on visitors’ browser type and/or other information. For more general information on cookies, please read “What Are Cookies” from Cookie Consent</p>
     
-    <h2>CCPA Privacy Rights (Do Not Sell My Personal Information)</h2>
+    <h5>CCPA Privacy Rights (Do Not Sell My Personal Information)</h5>
     <p>Under the CCPA, among other rights, California consumers have the right to:</p>
     <ul>
         <li>Request that a business that collects a consumer’s personal data disclose the categories and specific pieces of personal data that a business has collected about consumers.</li>
@@ -120,7 +120,7 @@ Like any other website, Habesha Tech Developers uses ‘cookies’. These cookie
     </ul>
     <p>If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us.</p>
     
-    <h2>GDPR Data Protection Rights</h2>
+    <h5>GDPR Data Protection Rights</h5>
     <p>We would like to make sure you are fully aware of all of your data protection rights. Every user is entitled to the following:</p>
     <ul>
         <li>The right to access – You have the right to request copies of your personal data. We may charge you a small fee for this service.</li>
@@ -132,7 +132,7 @@ Like any other website, Habesha Tech Developers uses ‘cookies’. These cookie
     </ul>
     <p>If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us.</p>
     
-    <h2>Children’s Information</h2>
+    <h5>Children’s Information</h5>
     <p>Another part of our priority is adding protection for children while using the internet. We encourage parents and guardians to observe, participate in, and/or monitor and guide their online activity.</p>
 
 <p>Habesha Tech Developers does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove such information from our records.</p>
